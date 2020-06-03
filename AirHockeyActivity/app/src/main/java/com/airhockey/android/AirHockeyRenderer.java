f324556c1f45220a2ab201c99538db25bd1e26cc
yu.xia <404638507@qq.com>
2020-06-03 23:41:05 +0800

    improve mallets

--- AirHockeyActivity/app/src/main/java/com/airhockey/android/AirHockeyRenderer.java
+++ AirHockeyActivity/app/src/main/java/com/airhockey/android/AirHockeyRenderer.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 
 import com.airhockey.android.objects.Mallet;
+import com.airhockey.android.objects.Puck;
 import com.airhockey.android.objects.Table;
 import com.airhockey.android.programs.ColorShaderProgram;
 import com.airhockey.android.programs.TextureShaderProgram;
@@ -20,6 +21,7 @@ import static android.opengl.GLES20.glViewport;
 import static android.opengl.Matrix.multiplyMM;
 import static android.opengl.Matrix.rotateM;
 import static android.opengl.Matrix.setIdentityM;
+import static android.opengl.Matrix.setLookAtM;
 import static android.opengl.Matrix.translateM;
 
 
@@ -28,9 +30,13 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     private final Context context;
     private final float[] projectionMatrix = new float[16];
     private final float[] modelMatrix = new float[16];
+    private final float[] viewMatrix = new float[16];
+    private final float[] viewProjectionMatrix = new float[16];
+    private final float[] modelViewProjectMatrix = new float[16];
 
     private Table table;
     private Mallet mallet;
+    private Puck puck;
 
     private TextureShaderProgram textureProgram;
     private ColorShaderProgram colorProgram;
@@ -46,7 +52,8 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
         table = new Table();
-        mallet = new Mallet();
+        mallet = new Mallet(0.08f, 0.15f, 32);
+        puck = new Puck(0.06f, 0.02f, 32);
 
         textureProgram = new TextureShaderProgram(context);
         colorProgram = new ColorShaderProgram(context);
@@ -59,27 +66,57 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         glViewport(0, 0, width, height);
         MatrixHelper.perspectiveM(projectionMatrix, 45, (float) width / (float) height, 1f, 10f);
 
-        setIdentityM(modelMatrix, 0);
-        translateM(modelMatrix, 0, 0f, 0f, -3f);
-        rotateM(modelMatrix, 0, -60f, 1f, 0f, 0f);
+        setLookAtM(viewMatrix, 0, 0f, 1.2f, 2.2f, 0f, 0f, 0f, 0f, 1f, 0f);
 
-        final float[] temp = new float[16];
-        multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0);
-        System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
+//        setIdentityM(modelMatrix, 0);
+//        translateM(modelMatrix, 0, 0f, 0f, -3f);
+//        rotateM(modelMatrix, 0, -60f, 1f, 0f, 0f);
+//
+//        final float[] temp = new float[16];
+//        multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0);
+//        System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Multiply the view and projection matrices together.
+        multiplyMM(viewProjectionMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+
+        positionTableInScene();
         textureProgram.useProgram();
-        textureProgram.setUniforms(projectionMatrix, texture);
+        textureProgram.setUniforms(modelViewProjectMatrix, texture);
         table.bindData(textureProgram);
         table.draw();
 
+        positionObjectInScene(0f, mallet.height / 2f, -0.4f);
         colorProgram.useProgram();
-        colorProgram.setUniforms(projectionMatrix);
+        colorProgram.setUniforms(modelViewProjectMatrix, 1f, 0f, 0f);
         mallet.bindData(colorProgram);
         mallet.draw();
+
+        positionObjectInScene(0f, mallet.height / 2f, 0.4f);
+        colorProgram.setUniforms(modelViewProjectMatrix, 0f, 0f, 1f);
+        mallet.draw();
+
+        positionObjectInScene(0f, puck.height / 2f, 0f);
+        colorProgram.setUniforms(modelViewProjectMatrix, 0.8f, 0.8f, 1.0f);
+        puck.bindData(colorProgram);
+        puck.draw();
+
+    }
+
+    private void positionObjectInScene(float x, float y, float z) {
+        setIdentityM(modelMatrix, 0);
+        translateM(modelMatrix, 0, x, y, z);
+        multiplyMM(modelViewProjectMatrix, 0, viewProjectionMatrix, 0, modelMatrix, 0);
+
+    }
+
+    private void positionTableInScene() {
+        setIdentityM(modelMatrix, 0);
+        rotateM(modelMatrix, 0, -90f, 1f, 0f, 0f);
+        multiplyMM(modelViewProjectMatrix, 0, viewProjectionMatrix, 0, modelMatrix, 0);
     }
 }
--- AirHockeyActivity/app/src/main/java/com/airhockey/android/objects/Puck.java
+++ AirHockeyActivity/app/src/main/java/com/airhockey/android/objects/Puck.java
@@ -6,35 +6,30 @@ import com.airhockey.android.util.Geometry;
 
 import java.util.List;
 
-public class Mallet {
-
+public class Puck {
     private static final int POSITION_COMPONENT_COUNT = 3;
-    public final float radius;
-    public final float height;
+
+    public final float radius, height;
 
     private final VertexArray vertexArray;
     private final List<ObjectBuilder.DrawCommand> drawList;
 
-    public Mallet(float radius, float height, int numPointsAroundMallet) {
-        ObjectBuilder.GeneratedData generatedData = ObjectBuilder.createMallet(new Geometry.Point(0f, 0f, 0f), radius
-                , height, numPointsAroundMallet);
+    public Puck(float radius, float height, int numPointsAroundPuck) {
+        ObjectBuilder.GeneratedData generatedData = ObjectBuilder.createPuck(new Geometry.Cylinder(new Geometry.Point(0f, 0f, 0f), radius, height), numPointsAroundPuck);
         this.radius = radius;
         this.height = height;
+
         vertexArray = new VertexArray(generatedData.vertexData);
         drawList = generatedData.drawList;
     }
 
     public void bindData(ColorShaderProgram colorProgram) {
-        vertexArray.setVertexAttribPointer(0,
-                colorProgram.getPositionAttributeLocation(),
-                POSITION_COMPONENT_COUNT,
-                0);
+        vertexArray.setVertexAttribPointer(0, colorProgram.getPositionAttributeLocation(), POSITION_COMPONENT_COUNT, 0);
     }
 
     public void draw() {
-        for (ObjectBuilder.DrawCommand command : drawList) {
-            command.draw();
+        for (ObjectBuilder.DrawCommand drawCommand : drawList) {
+            drawCommand.draw();
         }
     }
-
 }
